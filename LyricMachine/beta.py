a8b1ffeb6008cb989351d4dddf8f0e4a62b75ef6
IssacZerubbabel <issaczerubbabel@gmail.com>
2022-10-31 18:59:50 +0530

    LyricsOp.py
    Fetches and displays the lyrics when a song is played on VLC. This currently works for VLC ONLY

---
+++ LyricMachine/beta.py
@@ -0,0 +1,5 @@
+import lyricsgenius
+titl = open("C:\\Users\\Issac\\AppData\\Roaming\\vlc\\np_title.txt")
+artis = open("C:\\Users\\Issac\\AppData\\Roaming\\vlc\\np_artist.txt")
+print(artis.read(),"-",titl.read())
+
